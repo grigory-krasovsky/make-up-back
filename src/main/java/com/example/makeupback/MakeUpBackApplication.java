5c2309b6412a9dc7316aa51eb14801e3e8cd9588
GrigoryKrasovsky <grigory.krasovsky@gmail.com>
2024-04-29 18:22:30 +0300

    initial commit

---
+++ src/main/java/com/example/makeupback/MakeUpBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.makeupback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeUpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeUpBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/makeupback/MakeUpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makeupback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeUpBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
