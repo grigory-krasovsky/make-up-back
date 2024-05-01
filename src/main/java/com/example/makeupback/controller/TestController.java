30ffed8bf498fc9ff7059aae298f73527ce99fcc
GrigoryKrasovsky <grigory.krasovsky@gmail.com>
2024-05-01 16:23:46 +0300

    docker-compose fix controller

--- src/main/java/com/example/makeupback/controller/TestController.java
+++ src/main/java/com/example/makeupback/controller/TestController.java
@@ -3,9 +3,9 @@ package com.example.makeupback.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController()
 public class TestController {
-    @GetMapping
+    @GetMapping("/test")
     public String test() {
         return "Hello world";
     }
