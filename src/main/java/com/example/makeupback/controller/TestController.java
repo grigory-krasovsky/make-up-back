1008615ed250f6d64f00f57d3daed106640715e3
GrigoryKrasovsky <grigory.krasovsky@gmail.com>
2024-05-01 16:43:39 +0300

    ci cd first test

--- src/main/java/com/example/makeupback/controller/TestController.java
+++ src/main/java/com/example/makeupback/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "Hello world new version";
+        return "Hello world new version 2.0";
     }
 }
