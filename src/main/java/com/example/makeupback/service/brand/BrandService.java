7cdea5f13d77bad7f772031b7189f79ab06325c3
GrigoryKrasovsky <grigory.krasovsky@gmail.com>
2024-05-18 12:13:49 +0300

    fix brands

--- src/main/java/com/example/makeupback/service/brand/BrandService.java
+++ src/main/java/com/example/makeupback/service/brand/BrandService.java
@@ -2,11 +2,13 @@ package com.example.makeupback.service.brand;
 
 import com.example.makeupback.db.brand.BrandDto;
 import com.example.makeupback.db.brand.BrandRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class BrandService {
 
     private BrandRepository brandRepository;
