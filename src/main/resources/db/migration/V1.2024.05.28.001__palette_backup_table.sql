9789aaf8aa8d532e75e3f8deb2467862f199b398
GrigoryKrasovsky <grigory.krasovsky@gmail.com>
2024-05-28 18:43:50 +0300

    palette backup table

---
+++ src/main/resources/db/migration/V1.2024.05.28.001__palette_backup_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS palettes_backup (
+                                        id BIGINT PRIMARY KEY GENERATED  BY DEFAULT AS IDENTITY,
+                                        name TEXT,
+                                        price DECIMAL(10,2),
+                                        tag_id BIGINT,
+                                        image TEXT,
+                                        rating INT,
+                                        brand_id BIGINT,
+                                        description TEXT
+);
